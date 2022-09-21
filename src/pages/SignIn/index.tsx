ef3716942ef7fa585ebcc4b89bc79b1afe6f2822
rafael-rufino <rafaelrufino.52@gmail.com>
2022-09-21 12:39:05 -0300

    chore: atualizando a versao da biblioteca npm

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,32 +1,55 @@
 /* eslint-disable import/first */
 
-import React from 'react';
-import { createComponent } from '@lit-labs/react';
+import React from "react";
+import { createComponent } from "@lit-labs/react";
 
-import {Heading, Input, Button,LinkIcon, CheckText} from '@rafael-rufino/web-components-with-litelement-publish-in-npm';
+import {
+  Heading,
+  Input,
+  Button,
+  LinkIcon,
+  CheckText,
+} from "@rafael-rufino/web-components-with-litelement-publish-in-npm";
 
-import { Container, Wrapper } from './styles';
+import { Container, Wrapper } from "./styles";
 
-const HeadingApp = createComponent(React, 'heading-app', Heading);
-const InputApp = createComponent(React, 'input-app', Input);
-const ButtonApp = createComponent(React, 'button-app', Button);
-const LinkIconApp = createComponent(React, 'linkicon-app', LinkIcon);
-const CkeckTextApp= createComponent(React, 'checktext-app', CheckText);
+const HeadingApp = createComponent(React, "heading-app", Heading);
+const InputApp = createComponent(React, "input-app", Input);
+const ButtonApp = createComponent(React, "button-app", Button);
+const LinkIconApp = createComponent(React, "linkicon-app", LinkIcon);
+const CkeckTextApp = createComponent(React, "checktext-app", CheckText);
 
-import lockImg from '../../assets/lock.svg';
+import lockImg from "../../assets/lock.svg";
 
 export const SignIn = () => {
   return (
     <Container>
-        <Wrapper>
-        <HeadingApp title="Acessando sua conta" description='Informe seu login e senha para continuar' />
-        <InputApp label='Seu login' placeholder="E-mail, CPF, Usuário ou Celular" type='text'  onChange={() => {}}/>
-        <InputApp label='Sua senha' placeholder="Digite a sua senha"  type='password' onChange={() => {}} />
-        <CkeckTextApp name='Manter conectado' />
-        <ButtonApp type='submit' name='Acessar sua conta' onClick={()=>{}}/>
-            <LinkIconApp  src={lockImg} title="Você deseja redefinir a senha ?" href="https://www.gazin.com.br"  name='Esqueceu sua senha?'/>
-        </Wrapper>
+      <Wrapper>
+        <HeadingApp
+          title="Acessando sua conta"
+          description="Informe seu login e senha para continuar"
+        />
+        <InputApp
+          label="Seu login"
+          placeholder="E-mail, CPF, Usuário ou Celular"
+          type="text"
+          onChange={() => {}}
+        />
+        <InputApp
+          label="Sua senha"
+          placeholder="Digite a sua senha"
+          type="password"
+          onChange={() => {}}
+        />
+        <CkeckTextApp name="Manter conectado" />
+        <ButtonApp type="submit" name="Acessar sua conta" onClick={() => {}} />
+        <LinkIconApp
+          src={lockImg}
+          title="Você deseja redefinir a senha ?"
+          href="https://www.gazin.com.br"
+          name="Esqueceu sua senha?"
+        />
+      </Wrapper>
     </Container>
-  )
-}
-
+  );
+};
